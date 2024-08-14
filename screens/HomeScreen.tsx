d5a5892132636af39f149efddaad8bac89e3ddc1
nyambogahezron <hezzyn1@gmail.com>
2024-08-15 02:54:34 +0300

    home nav

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,33 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  Platform,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { styles } from '@/theme';
+
+const ios = Platform.OS == 'ios';
 
 const HomeScreen = () => {
   return (
     <View className='flex-1 bg-neutral-800'>
-      <SafeAreaView>
-        <Text>HomeScreen</Text>
+      {/* search bar and logo  */}
+      <SafeAreaView className={ios ? '-mb-2' : 'mb-3'}>
+        <View className='flex-row justify-between item-center mx-4'>
+          <Text className='text-white text-3xl font-bold'>
+            <Text style={styles.text}>M</Text>ovie
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size='30' strokeWidth={2} color='white' />
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
+
+      <StatusBar barStyle='light-content' />
     </View>
   );
 };
---
+++ theme/index.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  background: '#eab308',
+  text: '#eab308',
+};
+
+export const styles = {
+  text: { color: theme.text },
+  background: { background: theme.background },
+};
